9179bd0c3844ca3ecbe06d64e9c3a4b1a1fc4004
Armando García Paniagua <62965639+ArmandGP07@users.noreply.github.com>
2020-10-17 00:03:02 -0600

    Update MenuPrincipal.java

--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -1,1 +1,21 @@
+import javax.swing.*;
 
+
+public class MenuPrincipal extends JFrame{
+
+    public MenuPrincipal() {
+
+        JButton EditarPerfil = new JButton("Editar Perfil");
+        JButton Alquilar = new JButton("Alquilar");
+        JButton Entregar = new JButton("Entregar");
+        JButton Gestionar = new JButton("Gestionar");
+
+        EditarPerfil.addActionListener(Usuario./*etc*/);
+        Alquilar.addActionListener(Usuario./*etc*/);
+        Entregar.addActionListener(Usuario./*etc*/);
+        Gestionar.addActionListener(Usuario./*etc*/);
+
+    };
+
+
+}
